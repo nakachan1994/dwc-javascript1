ee06c9dadfeae25eeeabd1514dc1d9a13253cc47
yunakamura <nakachaaaan@gmail.com>
2021-04-17 16:52:20 +0900

    [finish]5-2

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-alert('Hello World');
+var max = 100;
+var num = 1;
+var count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて'　+ max + 'を超えるのに必要だった回数は' + count +'回です');
+var orange = 100;
+var apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+}else if(orrange == apple){
+  alert('みかんとりんごが同じ値段');
+}else{
+  alert('みかんの値段がりんごより安い')
+}
+
+
+
+
+
+
+
+
+
+var hello = 'Hello World';
+alert(hello);
+var int1 = 1;
+alert(int1);
+alert(4 + 3);
